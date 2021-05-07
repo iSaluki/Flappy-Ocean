c272711214d9476493f1fe28db8d5ed79e604bd0
Seth <seth@salukicorporation.com>
2021-05-07 20:23:15 +0100

    Support more input buttons

--- Game.gd
+++ Game.gd
@@ -6,23 +6,22 @@ var player = preload("res://Player.tscn").instance()
 var highScore
 
 # Called when the node enters the scene tree for the first time.
-#func _ready():
-#	player.connect("tree_exiting",self, "save")
-#	# Load high score
-#	var file = File.new()
-#	file.open("user://user.dat", File.READ)
-#	highScore = file.get_as_text()
-#	highScore = int(highScore)
-#	file.close()
-#	$HighScore.set_text(str(highScore))
+func _ready():
+	player.connect("tree_exiting",self, "save")
+	# Load high score
+	var file = File.new()
+	file.open("user://user.dat", File.READ)
+	highScore = file.get_as_text()
+	highScore = int(highScore)
+	file.close()
+	$HighScore.set_text(str(highScore))
 
-
-#func save():
-#	if score > highScore:
-#		var file = File.new()
-#		file.open("user://user.dat", File.READ)
-#		highScore = file.get_as_text()
-#		print(highScore)
+func save():
+	if score > highScore:
+		var file = File.new()
+		file.open("user://user.dat", File.READ)
+		highScore = file.get_as_text()
+		print(highScore)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
